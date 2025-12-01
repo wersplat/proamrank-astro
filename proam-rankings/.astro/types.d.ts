9dcec70046e4fc6186732873e0c77fd5c1ab983e
wersplat <wersplat@gmail.com>
2025-12-01 07:54:26 -0500

    Remove unused Clerk references from type definitions

--- proam-rankings/.astro/types.d.ts
+++ proam-rankings/.astro/types.d.ts
@@ -1,4 +1,2 @@
 /// <reference types="astro/client" />
-/// <reference path="integrations/_clerk_astro_integration/types.d.ts" />
 /// <reference path="content.d.ts" />
-/// <reference path="env.d.ts" />
